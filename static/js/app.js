9e0bc30777379fc783e2304dfb61a27a69cd3dd6
eduardprigoana <eduard@prigoana.com>
2025-11-23 12:00:05 +0200

    she say she from the islands

--- static/js/app.js
+++ static/js/app.js
@@ -76,6 +76,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     siteNameInput.addEventListener('input', function(e) {
         const siteName = e.target.value || 'YOUR_SITE_NAME';
-        webringCode.textContent = `<a href="${baseURL}/prev/${siteName}">←</a> ( <a href="${baseURL}">${siteTitle}</a> ) <a href="${baseURL}/rand/${siteName}">?</a> <a href="${baseURL}/next/${siteName}">→</a>`;
+        webringCode.textContent = `<a href="${baseURL}/prev/${siteName}">←</a>
+<a href="${baseURL}">${siteTitle}</a>
+<a href="${baseURL}/rand/${siteName}">?</a>
+<a href="${baseURL}/next/${siteName}">→</a>`;
     });
 });
